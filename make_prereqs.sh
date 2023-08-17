1614e25595063912bc68b61d0e0e4956f080f971
datedote <datadote@gmail.com>
2023-08-17 11:06:17 -0700

    updated reqs and add make_prereqs.sh

---
+++ make_prereqs.sh
@@ -0,0 +1,4 @@
+jupyter nbconvert --to python 01_matrix_fact_simple.ipynb
+jupyter nbconvert --to python 02_matrix_fact_advanced.ipynb
+jupyter nbconvert --to python 03_factorization_machines.ipynb
+pipreqs --force .
